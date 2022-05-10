f8965e52031433ce6e344d60bed372b0d4790e8a
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-05-10 15:45:46 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\Api\V1\Admin\ProjectApiController;
 use App\Http\Controllers\Auth\GenerateTokenController;
 use App\Http\Controllers\Api\V1\Admin\StakedTokensToProjectApiController;
+use App\Http\Controllers\Api\V1\Admin\OwnerContractInformation;
 
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'middleware' => ['auth:sanctum']], function () {
+
+    Route::get('owner', OwnerContractInformation::class);
     // Projects
     Route::apiResource('projects', ProjectApiController::class, ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
 
